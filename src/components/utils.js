df10c16d3f3082387a345e925c0c0b1b6e8a7ffd
Development - CharCoin <development@charcoin.org>
2025-08-04 03:15:48 +0530

    API Url updated.

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,5 @@
 
-export const apiUrl =    "http://localhost:8000"; //"https://dapp.charcoin.org";
+export const apiUrl =    "http://31.97.150.58:8000";
 
 
 export const toPeriod=(totalSeconds)=> {
